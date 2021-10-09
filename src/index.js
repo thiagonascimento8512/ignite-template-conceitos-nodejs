7c83c8261269ec1f4d4d3cc0b56030217ba1f61e
Thiago Nascimento <thiago.nascimento8512@gmail.com>
2021-10-09 00:10:23 -0300

    Implemented mark a todo as done

--- src/index.js
+++ src/index.js
@@ -87,7 +87,18 @@ app.put("/todos/:id", checksExistsUserAccount, (request, response) => {
 });
 
 app.patch("/todos/:id/done", checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: "Todo does not exists!" });
+  }
+
+  todo.done = true;
+
+  return response.status(201).json(todo);
 });
 
 app.delete("/todos/:id", checksExistsUserAccount, (request, response) => {
